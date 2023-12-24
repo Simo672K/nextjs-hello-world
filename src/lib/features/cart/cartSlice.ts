4774a9ecf79c18711b675738aac1744347a8676a
Simo672k <mohammed.hakmiii555@gmail.com>
2023-12-24 18:26:47 +0100

    redux store initialization, and cart slice

---
+++ src/lib/features/cart/cartSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "@/lib/store";
+import { StaticImageData } from "next/image";
+
+type CartItem = {
+  id: number;
+  title: string;
+  image: StaticImageData;
+  quantity: number;
+};
+
+// Define a type for the slice state
+interface CartState {
+  items: Array<CartItem>;
+  numberOfCartItems: 0;
+}
+
+// Define the initial state using that type
+const initialState: CartState = {
+  items: [],
+  numberOfCartItems:0
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<CartItem>) => {
+      state.items.push(action.payload);
+      state.numberOfCartItems++;
+    },
+    deleteItem: (state, action: PayloadAction<CartItem>) => {
+      state.items.filter(item=> item.id !== action.payload.id);
+      state.numberOfCartItems++;
+    },
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    incrementItemQuantity: (state, action: PayloadAction<number>) => {
+      const item= state.items.find(item=> item.id === action.payload) as CartItem;
+      item.quantity++;
+      state.numberOfCartItems++;
+      
+    },
+    decrementItemQuantity: (state, action: PayloadAction<number>) => {
+      const item= state.items.find(item=> item.id === action.payload) as CartItem;
+      item.quantity--;
+      state.numberOfCartItems++;
+      
+    },
+    resetCart: (state) =>{
+      state= initialState;
+    }
+  },
+});
+
+export const { addItem, deleteItem, incrementItemQuantity } = cartSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectItems = (state: RootState) => state.cart.items;
+export const selectnumberOfCartItems = (state: RootState) => state.cart.numberOfCartItems;
+
+export default cartSlice.reducer;
